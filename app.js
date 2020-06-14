fa12f67e84da10981eeac3a163e9fc88a01d268b
Hugh Millear <hughmillear@gmail.com>
2020-06-14 20:32:26 +0200

    default express setup

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 //jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs"); // eslint-disable-line
+
+const app = express();
+
+app.use(expre.static("public"));
+app.set('view engine'. 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  if (process.platform === "linux") {
+    port = 3001;
+  } else {
+    port = 3000;
+  }
+}
+
+
+
+
+
+app.listen(port, function () {
+    console.log(`Server started at: http://localhost:${port}`);
+  });
