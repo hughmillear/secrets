830210dd8c66e43996f027eab69dc58a808ff6f5
Hugh Millear <hughmillear@gmail.com>
2020-06-15 21:10:43 +0200

    passport integration for local auth. two options for login, the verbose and not-verbose options

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs"); // eslint-disable-line
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.use(
   bodyParser.urlencoded({
@@ -30,6 +32,16 @@ if (port == null || port == "") {
   }
 }
 
+app.use(
+  session({
+    secret: process.env.SECRET_STRING,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 let table_name = "userDB";
 
 let node_env = process.env.NODE_ENV || "development";
@@ -45,19 +57,19 @@ if (node_env === "production") {
     useUnifiedTopology: true,
   });
 }
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+  email: String,
+  password: String,
 });
 
+userSchema.plugin(passportLocalMongoose);
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
   res.render("home");
@@ -71,56 +83,60 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
 app.post("/register", function (req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+  User.register({ username: req.body.username }, req.body.password, function (
+    err,
+    user
+  ) {
     if (err) {
       console.log(err);
-      res.sendStatus(500);
-    } else {
-      const newUser = new User({
-        email: req.body.username,
-        password: hash,
-      });
+      return res.redirect("/register");
+    }
+    console.log(`user: ${user}`);
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/secrets");
+    });
+  });
+});
 
-      newUser.save(function (err) {
-        if (err) {
-          console.log(err);
-          res.sendStatus(500);
-        } else {
-          res.render("secrets");
-        }
+// app.post(
+//   "/login",
+//   passport.authenticate("local", {
+//     successRedirect: "/secrets",
+//     failureRedirect: "/login",
+//   })
+// );
+
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+      console.log(`Failed login for user: ${req.body.username}`);
+      res.redirect("/login");
+    } else {
+      console.log(`Succesful login for user: ${req.body.username}`);
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
       });
     }
   });
 });
 
-app.post("/login", function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({ email: username }, function (err, foundUser) {
-    if (err) {
-      console.log(err);
-      res.sendStatus(500);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function (err, result) {
-          if (err) {
-            console.log(err);
-            res.sendStatus(500);
-          } else {
-            console.log(result);
-            if (result === true) {
-              res.render("secrets");
-            } else {
-              console.log("wrong password");
-              res.redirect("/login");
-            }
-          }
-        });
-      }
-    }
-  });
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
 app.listen(port, function () {
--- docs.md
+++ docs.md
@@ -23,3 +23,12 @@ Plaintext password in database.
   - bcrypt is 17k/sec on GPU, MD5 is 20B/sec
   - Can also enable multiple salt rounds
   - Salt rounds are str+salt->hash, then do hash+salt->hash2 'n' number of times
+
+## Level 5
+
+- Cookies and Session
+  - Session cookies relate only to the current user session on the site.
+- Passport is a popular library for Node
+- Not great documentation...
+- Remember that MongoDB doesn't care about the schema!
+  - Hashes and salts are not part of the model, but are part of the document saved. Username and passowrd can't be required, as they aren't always filled on MongoDB document creation.
